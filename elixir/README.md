1e2091b646f66830f5b39e0bf4cab3ed6e18c8c5
Julien Crevits <julien.crevits@gengo.com>
2020-03-08 16:54:03 +0900

    elixir course - sections 1-3

---
+++ elixir/README.md
@@ -0,0 +1,1 @@
+Studying Elixir with [The Complete Elixir and Phoenix Bootcamp (Udemy)](https://www.udemy.com/course/the-complete-elixir-and-phoenix-bootcamp-and-tutorial/)
---
+++ elixir/cards/lib/cards.ex
@@ -0,0 +1,60 @@
+defmodule Cards do
+  @moduledoc """
+  Documentation for `Cards`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Cards.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+
+  def create_deck do
+    values = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"]
+    suits = ["Clubs", "Diamonds", "Hearts", "Spades"]
+
+    cards = for suit <- suits, value <- values do 
+      "#{value} of #{suit}"
+    end
+
+    List.flatten(cards)
+  end
+
+  def shuffle(deck) do
+    Enum.shuffle(deck)
+  end
+
+  def contains?(deck, card) do
+    Enum.member?(deck, card)
+  end
+
+  def deal(deck, size) do
+    Enum.split(deck, size)
+  end
+
+  def save(deck, filename) do
+    binary = :erlang.term_to_binary(deck)
+    File.write(filename, binary)
+  end
+
+  def load(filename) do
+    case File.read(filename) do
+      {:ok, binary} -> :erlang.binary_to_term(binary)
+      {:error, _reason} -> "That file does not exist"
+    end
+  end
+
+  def create_hand(hand_size) do
+    Cards.create_deck
+    |> Cards.shuffle
+    |> Cards.deal(hand_size)
+  end
+
+end
