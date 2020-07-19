f3fbd6aa8193892548eed0ef8082670ce37d8b20
Julien Crevits <julien.crevits@gengo.com>
2020-07-19 13:34:32 +0900

    zoom clone - copy/paste a lot of code

--- elixir/zoom/assets/js/app.js
+++ elixir/zoom/assets/js/app.js
@@ -19,7 +19,41 @@ import { LiveSocket } from "phoenix_live_view"
 
 let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
 
-let localStream = null
+let Hooks = {}
+
+// USER JOINS/LEAVES
+
+let users = []
+
+function addUserConnection(userUuid) {
+  if (users[userUuid] === undefined) {
+    users[userUuid] = {
+      peerConnection: null
+    }
+  }
+
+  return users
+}
+
+function removeUserConnection(userUuid) {
+  delete users[userUuid]
+
+  return users
+}
+
+Hooks.InitUser = {
+  mounted() {
+    addUserConnection(this.el.dataset.userUuid)
+  },
+
+  destroyed() {
+    removeUserConnection(this.el.dataset.userUuid)
+  }
+}
+
+// JOIN CALL
+
+let localStream
 
 async function initStream() {
   try {
@@ -34,10 +68,124 @@ async function initStream() {
   }
 }
 
-let Hooks = {}
 Hooks.JoinCall = {
   mounted() {
     this.el.addEventListener('click', e => { initStream() })
+  }
+}
+
+// lv       - Our LiveView hook's `this` object.
+// fromUser - The user to create the peer connection with.
+// offer    - Stores an SDP offer if it was passed to the function.
+function createPeerConnection(lv, fromUser, offer) {
+  // Creates a variable for our peer connection to reference within
+  // this function's scope.
+  let newPeerConnection = new RTCPeerConnection({
+    iceServers: [
+      // We're going to get into STUN servers later, but for now, you
+      // may use ours for this portion of development.
+      { urls: "stun:littlechat.app:3478" }
+    ]
+  })
+
+  // Add this new peer connection to our `users` object.
+  users[fromUser].peerConnection = newPeerConnection;
+
+  // Add each local track to the RTCPeerConnection.
+  localStream.getTracks().forEach(track => newPeerConnection.addTrack(track, localStream))
+
+  // If creating an answer, rather than an initial offer.
+  if (offer !== undefined) {
+    newPeerConnection.setRemoteDescription({ type: "offer", sdp: offer })
+    newPeerConnection.createAnswer()
+      .then((answer) => {
+        newPeerConnection.setLocalDescription(answer)
+        console.log("Sending this ANSWER to the requester:", answer)
+        lv.pushEvent("new_answer", { toUser: fromUser, description: answer })
+      })
+      .catch((err) => console.log(err))
+  }
+
+  newPeerConnection.onicecandidate = async ({ candidate }) => {
+    // fromUser is the new value for toUser because we're sending this data back
+    // to the sender
+    lv.pushEvent("new_ice_candidate", { toUser: fromUser, candidate })
+  }
+
+  // Don't add the `onnegotiationneeded` callback when creating an answer due to
+  // a bug in Chrome's implementation of WebRTC.
+  if (offer === undefined) {
+    newPeerConnection.onnegotiationneeded = async () => {
+      try {
+        newPeerConnection.createOffer()
+          .then((offer) => {
+            newPeerConnection.setLocalDescription(offer)
+            console.log("Sending this OFFER to the requester:", offer)
+            lv.pushEvent("new_sdp_offer", { toUser: fromUser, description: offer })
+          })
+          .catch((err) => console.log(err))
+      }
+      catch (error) {
+        console.log(error)
+      }
+    }
+  }
+
+  // When the data is ready to flow, add it to the correct video.
+  newPeerConnection.ontrack = async (event) => {
+    console.log("Track received:", event)
+    document.getElementById(`video-remote-${fromUser}`).srcObject = event.streams[0]
+  }
+
+  return newPeerConnection;
+}
+
+Hooks.HandleOfferRequest = {
+  mounted() {
+    console.log("new offer request from", this.el.dataset.fromUserUuid)
+    let fromUser = this.el.dataset.fromUserUuid
+    createPeerConnection(this, fromUser)
+  }
+}
+
+Hooks.HandleIceCandidateOffer = {
+  mounted() {
+    let data = this.el.dataset
+    let fromUser = data.fromUserUuid
+    let iceCandidate = JSON.parse(data.iceCandidate)
+    let peerConnection = users[fromUser].peerConnection
+
+    console.log("new ice candidate from", fromUser, iceCandidate)
+
+    peerConnection.addIceCandidate(iceCandidate)
+  }
+}
+
+Hooks.HandleSdpOffer = {
+  mounted() {
+    let data = this.el.dataset
+    let fromUser = data.fromUserUuid
+    let sdp = data.sdp
+
+    if (sdp != "") {
+      console.log("new sdp OFFER from", data.fromUserUuid, data.sdp)
+
+      createPeerConnection(this, fromUser, sdp)
+    }
+  }
+}
+
+Hooks.HandleAnswer = {
+  mounted() {
+    let data = this.el.dataset
+    let fromUser = data.fromUserUuid
+    let sdp = data.sdp
+    let peerConnection = users[fromUser].peerConnection
+
+    if (sdp != "") {
+      console.log("new sdp ANSWER from", fromUser, sdp)
+      peerConnection.setRemoteDescription({ type: "answer", sdp: sdp })
+    }
   }
 }
 
