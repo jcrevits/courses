dd4f827ac084c9465970e1423e68c804b5ae32a3
Julien Crevits <julien.crevits@gengo.com>
2020-08-27 16:54:19 +0900

    fun with algos - sections 12 to 19

--- AlgoCasts/exercises/vowels/index.js
+++ AlgoCasts/exercises/vowels/index.js
@@ -7,6 +7,23 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+const vowelArray = ["a", "e", "i", "o", "u"]
+
+function vowels(str) {
+  const matches = str.match(/[aieou]/gi);
+  return matches ? matches.length : 0;
+}
+
+// function vowels(str) {
+//   let counter = 0;
+
+//   for (let c of str.toLowerCase()) {
+//     if (vowelArray.includes(c)) {
+//       counter++;
+//     }
+//   }
+
+//   return counter;
+// }
 
 module.exports = vowels;
